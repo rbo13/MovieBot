f5b11354079a029789ab8071df08a57d967d2788
whaangbuu <chardy.orofeo@yahoo.com.ph>
2017-07-08 08:26:24 +0800

    Testing bot

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.get('/', function(req, res) {
   res.send("Howdy! I am a chatbot!");
 });
 
+// Facebook page token
+const token = "EAAcAuxG36YUBAMg9dJYD4lbNqNUS2o4p8fCTZCUA7oKSbDiuFw4JeCa3CkpOZBBwhZCbQDHbTJaob1cw9QorfPII91LOyAezV0tZAqgMLjn5hS2Xq1TcteFdi3N41dk55TLMPFRfexqCVazyP2urFU5OCZAUWt7RTke3uHoOpGwFdONRY2Xy8"
+
 // Facebook
 app.get('/webhook/', function(req, res) {
   if(req.query['hub.verify_token'] === 'someTokenHere') {
@@ -25,6 +28,42 @@ app.get('/webhook/', function(req, res) {
 
   res.send("Wrong token");
 });
+
+// SEND Facebook
+app.post('/webhook/', function(req, res) {
+  let messaging_events = req.body.entry[0].messaging_events;
+
+  for(let i = 0; i < messaging_events.length; i++) {
+    let event = messaging_events[i];
+    let sender = event.sender.id;
+
+    if(event.message && event.message.text) {
+      let text = event.message.text;
+      sendMessage(sender, "Text echo: " +text.substring(0, 100))
+    }
+  }
+  res.sendStatus(200)
+});
+
+// Send messag back.
+function sendMessage(sender, message) {
+  let messageData = { text: message };
+  request({
+    url: "https://graph.facebook.com/v2.6/me/messages",
+    qs: { access_token: token },
+    method: "POST",
+    json: {
+      receipt: { id: sender },
+      message: messageData
+    }
+  }, function(error, response, body) {
+    if(error) {
+      throw error;
+    }else if(response.body.error) {
+      throw response.body.error
+    }
+  })
+}
 
 app.listen(app.get('port'), function() {
   console.log("Running on port: +", app.get("port"));
