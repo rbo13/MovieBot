1019e07f44ea0af8e98c95f8438861aece87d37e
whaangbuu <chardy.orofeo@yahoo.com.ph>
2017-07-08 08:34:14 +0800

    Fixing

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ function sendMessage(sender, message) {
     qs: { access_token: token },
     method: "POST",
     json: {
-      receipt: { id: sender },
+      recipient: { id: sender },
       message: messageData
     }
   }, function(error, response, body) {
